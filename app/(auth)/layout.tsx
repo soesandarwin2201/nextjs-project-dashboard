031867e20b8b1575cb8073d391d73cd035f6036f
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-05-22 11:07:35 +0630

    Create the button andtailwind config

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+import "@/styles/global.css";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({
+  variable: "--font-inter",
+});
+
+export default function AuthRootLayout({ children }) {
+  return (
+    <html lang="en" className={inter.variable}>
+      <head />
+      <body className="h-screen w-screen rainbow-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center justify-center">
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  );
+}
